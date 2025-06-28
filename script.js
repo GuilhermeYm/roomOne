fa05f43e9bcfff434924daea8fdb507850bd7a69
Guilherme <guilhermeym2009@gmail.com>
2025-06-27 21:08:06 -0300

    ...

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ button.addEventListener('click', function(event) {
     const key = input.value.trim(); // Get the value of the input field
 
     if (key === '06/23/2025') { // Check if the key is correct
-        window.location.href = '../roomTwo/index.html'; // Redirect to the next room
+        alert('https://drive.google.com/drive/folders/1dsaRtJqXm4-rhTIchXi_gxvKIkDaS8rn?usp=sharing') // Redirect to the next room
     } else {
         alert('Senha incorreta! Tente novamente.'); // Alert for incorrect password
     }
